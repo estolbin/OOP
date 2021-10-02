a7c0caa30318a69ca264ded671703417f3983ec4
Eugene Stolbin <estolbin@gmail.com>
2021-10-02 14:25:59 +0300

    Constructors in progress

--- IntroductionToOOP/IntroductionToOOP/main.cpp
+++ IntroductionToOOP/IntroductionToOOP/main.cpp
@@ -24,17 +24,49 @@ public:
 	{
 		this->y = y;
 	}
-	double distance_to(Point N)
+
+	// Constructors
+	//Point()
+	//{
+	//	x = y = 0;
+	//	cout << "DefaultConstructor:\t" << this << endl;
+	//}
+	Point(double x = 0, double y = 0)
+	{
+		this->x = x;
+		this->y = y;
+		cout << "Constructor:\t\t" << this << endl;
+	}
+	Point(const Point& other)
+	{
+		this->x = other.x;
+		this->y = other.y;
+		cout << "CopyConstructor:\t" << this << endl;
+	}
+	~Point()
+	{
+		cout << "Destructor:\t\t" << this << endl;
+	}
+
+
+	// Methods
+	double distance_to(const Point& N) const
 	{
 		double length_x = this->x - N.get_x();
 		double length_y = this->y - N.get_y();
 		return sqrt(pow(length_x, 2) + pow(length_y, 2));
 	}
+	void print()const
+	{
+		cout << "X = " << x << tab << "Y = " << y << endl;
+	}
 };
 
-double distance(Point a, Point b);
+double distance(const Point& a, const Point& b);
 
 //#define STRUCT_POINT
+#define DISTANCE
+//#define CONSTRUCTORS_CHECK
 
 void main()
 {
@@ -49,6 +81,7 @@ void main()
 	cout << (*pA).x << tab << (*pA).y << endl;
 #endif // STRUCT_POINT
 
+#ifdef DISTANCE
 	Point A;
 
 	A.set_x(2);
@@ -61,11 +94,32 @@ void main()
 	B.set_y(5);
 	//distance - расстояние до указанной точки
 
-	cout << "Расстояние от точки А до точки B:" << distance(A, B) << endl;
-	cout << "Расстояние от точки B до точки A:" << B.distance_to(A) << endl;
+	cout << "\n------------------------------------\n";
+	cout << distance(A, B) << endl;
+	cout << "\n------------------------------------\n";
+	cout << distance(B, A) << endl;
+	cout << "\n------------------------------------\n";
+	cout << B.distance_to(A) << endl;
+	cout << "\n------------------------------------\n";
+	cout << A.distance_to(B) << endl;
+	cout << "\n------------------------------------\n";
+#endif
+
+#ifdef CONSTRUCTORS_CHECK
+	Point A; // DefaultConstructor
+	A.print();
+	Point B(2, 3);
+	B.print();
+	Point C = B;
+	C.print();
+
+	Point D(A);
+	D.print();
+#endif // CONSTRUCTORS_CHECK
+
 }
 
-double distance(Point a, Point b)
+double distance(const Point& a, const Point& b)
 {
 	double length_x = b.get_x() - a.get_x();
 	double length_y = b.get_y() - a.get_y();
