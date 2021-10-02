165b8a63093e8e7f547c52616c91b3ece5d9688e
Eugene Stolbin <estolbin@gmail.com>
2021-10-02 09:34:15 +0300

    In progress

--- IntroductionToOOP/IntroductionToOOP/main.cpp
+++ IntroductionToOOP/IntroductionToOOP/main.cpp
@@ -1,10 +1,29 @@
-#include<iostream>
-
+﻿#include<iostream>
 using namespace std;
+#define tab "\t"
+
+class Point
+{
+	double x;
+	double y;
+public:
+};
+
+#define STRUCT_POINT
 
 void main()
 {
 	setlocale(LC_ALL, "Russian");
+#ifdef STRUCT_POINT
+	Point A;
+	A.x = 2; A.y = 3;
+	cout << A.x << tab << A.y << endl;
+
+	Point* pA = &A;
+	cout << pA->x << tab << pA->y << endl;
+	cout << (*pA).x << tab << (*pA).y << endl;
+#endif // STRUCT_POINT
+
 
 
 }
