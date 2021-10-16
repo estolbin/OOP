3f93b9a357c678012d4ea89fecc2df6ce87945f8
Eugene Stolbin <estolbin@gmail.com>
2021-10-16 14:22:02 +0300

    Overloading operators

--- IntroductionToOOP/IntroductionToOOP/main.cpp
+++ IntroductionToOOP/IntroductionToOOP/main.cpp
@@ -61,6 +61,27 @@ public:
 		return *this;
 	}
 
+	Point& operator+=(const Point& other)
+	{
+		this->x += other.x;
+		this->y += other.y;
+		return *this;
+	}
+
+	// Increment/Decrement
+	Point& operator++() // prefix increment
+	{
+		this->x++;
+		this->y++;
+		return *this;
+	}
+	Point operator++(int) // suffix increment
+	{
+		Point old = *this;
+		this->x++;
+		this->y++;
+		return old;
+	}
 
 	// Methods
 	double distance_to(const Point& N) const
@@ -75,13 +96,39 @@ public:
 	}
 };
 
+Point operator+(const Point& left, const Point& right)
+{
+	Point res;
+	res.set_x(left.get_x() + right.get_x());
+	res.set_y(left.get_y() + right.get_y());
+	return res;
+}
+
+Point operator-(const Point& left, const Point& right)
+{
+	Point res(left.get_x() - right.get_x(), left.get_y() - right.get_y());
+	return res;
+}
+
+bool operator==(const Point& left, const Point& right)
+{
+	if (left.get_x() == right.get_x() && left.get_y() == right.get_y())
+		return true;
+	else
+		return false;
+}
+
 double distance(const Point& a, const Point& b);
 
 #define delimeter "\n------------------------------------\n"
 
+
+
 //#define STRUCT_POINT
 //#define DISTANCE
 //#define CONSTRUCTORS_CHECK
+//#define ASSIGNMENT_CHECK
+//#define ARITHMETICAL_OPERATORS
 
 void main()
 {
@@ -138,6 +185,7 @@ void main()
 	A.print();
 #endif // CONSTRUCTORS_CHECK
 
+#ifdef ASSIGNMENT_CHECK
 	Point A, B, C;
 	cout << delimeter << endl;
 	A = B = C = Point(17, 18);
@@ -145,7 +193,35 @@ void main()
 	A.print();
 	B.print();
 	C.print();
+#endif // ASSIGNMENT_CHECK
+	
+#ifdef ARITHMETICAL_OPERATORS
+	Point A(2, 3);
+	Point B(3, 4);
+	A += B;
+	A.print();
+	Point C = A + B;
+	C.print();
+	C = A - B;
+	C.print();
+	++C;
+	C.print();
+	Point D = C++;
+	C.print();
+	D.print();
+#endif // ARITHMETICAL_OPERATORS
 
+	Point A(2, 3);
+	//Point B(3, 4);
+	Point B = A;
+	if (A == B++)
+	{
+		cout << "Точки равны" << endl;
+	}
+	else
+	{
+		cout << "Точки разные" << endl;
+	}
 }
 
 double distance(const Point& a, const Point& b)
